9b19b914f1c74e115cef97f3262e33cd9282cd70
saied lakhdar <saied.loai@gmail.com>
2018-02-24 23:16:02 +0100

    first commit

---
+++ doctor/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/app.css">
+  <link rel="stylesheet" href="../css/fontawesome.min.css">
+
+  <title>Espace Docteur</title>
+</head>
+<body>
+
+<div class="conainer-fluid">
+<!--
+@ this template build it by saied lakhdar loai > Djelfanetwork
+@ its free to use ^^
+@ fb.com/Djelfanetwork
+@ fb.com/saiedlakhdar
+@ t.co/saied.lakhdar
+-->
+<!--[navbar start]-->
+<nav class="navbar navbar-expand navbar-dark bg-info">
+  <a href="#" class="sidebar-toggle text-light mr-1"><i class="fa fa-bars"></i></a>
+  <a href="" class="navbar-brand"><i class="fa fa-code-branch"></i> Navbar</a>
+  <div class="navbar-collapse collapse "></div>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item dropdown">
+      <a href="#" class="nav-link dropdown-toggle" id="navbardropdown" data-toggle="dropdown">
+      <i class="fa fa-user"></i> Username
+      </a>
+      <div class="dropdown-menu dropdown-menu-left " aria-lebelleddby="navbardropdown">
+        <a href="#" class="dropdown-item">Action</a>
+        <a href="#" class="dropdown-item">Anchor</a>
+      </div>
+      </li>
+    </ul>
+
+</nav>
+<!--[navbar end ]-->
+
+<!--[sidebar  start]-->
+<div class="d-flex">
+  <nav class="sidebar bg-dark">
+    <ul class="list-unstyled">
+      <li><a href=""><i class="fa fa-fw fa-link"></i> Sidebar link</a></li>
+      <li><a href=""><i class="fa fa-fw fa-link"></i> Sidebar link</a></li>
+      <li>
+        <a href="#submenu1" data-toggle="collapse"><i class="fa fa-fw fa-address-card"></i> Dropdown link</a>
+        <ul id="submenu1" class="list-unstyled collapse">
+          <li><a href="#">Submenu item</a></li>
+          <li><a href="#">Submenu item</a></li>
+          <li><a href="#">Submenu item</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#submenu2" data-toggle="collapse"><i class="fa fa-fw fa-address-card"></i> Dropdown link</a>
+        <ul id="submenu2" class="list-unstyled collapse">
+          <li><a href="#">Submenu item</a></li>
+          <li><a href="#">Submenu item</a></li>
+          <li><a href="#">Submenu item</a></li>
+        </ul>
+      </li>
+      <li><a href="#"><i class="fa fa-fw fa-angle-right"></i> Sidebar Link </a></li>
+      <li><a href="#"><i class="fa fa-fw fa-link"></i> Sidebar Link </a></li>
+      <li><a href="#"><i class="fa fa-fw fa-link"></i> Sidebar Link </a></li>
+      <li><a href="#"><i class="fa fa-fw fa-link"></i> Sidebar Link </a></li>
+      <li><a href="#"><i class="fa fa-fw fa-link"></i> Sidebar Link </a></li>
+    </ul>
+  </nav>
+</div>
+<!--[sidebar  end]-->
+
+</div>
+
+
+<script src="../js/jquery.min.js" charset="utf-8"></script>
+<script src="../js/popper.min.js" charset="utf-8"></script>
+<script src="../js/bootstrap.min.js" charset="utf-8"></script>
+<script src="../js/app.js" charset="utf-8"></script>D
+</body>
+</html>
